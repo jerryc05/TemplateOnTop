9365f429d1fd9f6f6fd7dcaba2b0d634a2c66505
Jerry Chen <jerryc443@gmail.com>
2023-12-28 09:50:35 -0500

    Add React Quill component and dependencies

---
+++ src/components/ Quill.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+export function Quill() {
+  const [value, setValue] = useState('')
+  return <ReactQuill theme='snow' value={value} onChange={setValue} />
+}
