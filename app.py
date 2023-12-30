f5be32a09cb2320451961f9b340608d84a73dc3d
Jerry Chen <jerryc443@gmail.com>
2023-12-30 16:09:13 -0500

    Add dependencies and generate TypeScript client

---
+++ app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from topmost import get_visible_windows, WindowInfo
+
+#
+#
+#
+
+api = FastAPI(servers=[{"url": "/api"}])
+api.openapi_version = "3.0.3"
+
+app = FastAPI()
+app.mount(api.servers[0]["url"], api)
+
+#
+#
+#
+
+working_dir = Path(__file__).parent.absolute()
+frontend_dist = working_dir / "dist"
+if frontend_dist.is_dir():
+    app.mount(
+        "/",
+        StaticFiles(directory=frontend_dist, html=True),
+    )
+else:
+    raise RuntimeError(f"Frontend dist directory {frontend_dist} does not exist!")
+
+#
+#
+#
+
+
+@api.post("/windows")
+def visible_windows() -> "list[WindowInfo]":
+    return get_visible_windows()
+
+
+with open(working_dir / "openapi.json", "w") as f:
+    json.dump(obj=api.openapi(), fp=f)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if sys.argv[1:] == ["--dry-run"]:
+        sys.exit(0)
+
+    import os
+    import socket
+    import uvicorn
+    import netifaces
+
+    port = int(os.getenv("PORT", 8080))
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(0.2)
+        if sock.connect_ex(("127.0.0.1", port)) == 0:
+            raise RuntimeError(f"Port {port} is already in use!")
+
+    uvicorn.run(  # pyright: ignore[reportUnknownMemberType]
+        f"{Path(__file__).stem}:{f'{app=}'.split('=')[0]}", log_level="info"
+    )
--- topmost.py
+++ topmost.py
@@ -1,60 +1,70 @@
 from dataclasses import dataclass
-from os import name
 from typing import cast
 
 import psutil
-import win32con
-import win32gui
-import win32process
+from pydantic import BaseModel, Field
 
-
-def get_hwnd_topmost(hwnd: int):
-    return (
-        cast(int, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE))
-        & win32con.WS_EX_TOPMOST
-    )
-
-
-def set_hwnd_topmost(hwnd: int, is_top):
-    win32gui.SetWindowPos(
-        hwnd,
-        win32con.HWND_TOPMOST if is_top else win32con.HWND_NOTOPMOST,
-        0,
-        0,
-        0,
-        0,
-        win32con.SWP_NOMOVE | win32con.SWP_NOSIZE,
-    )
+try:
+    import win32con
+    import win32gui
+    import win32process
+except Exception:
+    if psutil.WINDOWS:
+        raise
 
 
 @dataclass
-class WindowInfo:
+class WindowInfo(BaseModel):
     hwnd: int
     title: str
     is_top: int
-    name_of_pid: "str|None"
-    exe_of_pid: "str|None"
+    name_of_pid: "str|None" = Field(None)
+    exe_of_pid: "str|None" = Field(None)
+
+    def set_topmost(self, is_top: bool):
+        win32gui.SetWindowPos(
+            self.hwnd,
+            win32con.HWND_TOPMOST if is_top else win32con.HWND_NOTOPMOST,
+            0,
+            0,
+            0,
+            0,
+            win32con.SWP_NOMOVE | win32con.SWP_NOSIZE,
+        )
+
+    def get_topmost(self):
+        return self.__class__.get_hwnd_topmost(hwnd=self.hwnd)
+
+    @staticmethod
+    def get_hwnd_topmost(hwnd: int):
+        return (
+            cast(int, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE))
+            & win32con.WS_EX_TOPMOST
+        )
 
 
-def enum_windows_callback(hwnd: int, resultList: "list[WindowInfo]"):
-    # append tuple of hwnd and window title to resultList
-    title = win32gui.GetWindowText(hwnd)
-    if not title or title in ("Default IME", "MSCTFIME UI", "Widgets"):
+def enum_windows_callback(hwnd: int, result: "list[WindowInfo]"):
+    if not win32gui.IsWindowVisible(hwnd):
         return
+
+    title = win32gui.GetWindowText(hwnd)
+
     _tid, pid = win32process.GetWindowThreadProcessId(hwnd)
     try:
         name_of_pid = psutil.Process(pid).name()
     except Exception:
         name_of_pid = None
+
     try:
         exe_of_pid = psutil.Process(pid).exe()
     except Exception:
         exe_of_pid = None
-    resultList.append(
+
+    result.append(
         WindowInfo(
             hwnd=hwnd,
             title=title,
-            is_top=get_hwnd_topmost(hwnd=hwnd),
+            is_top=WindowInfo.get_hwnd_topmost(hwnd=hwnd),
             name_of_pid=name_of_pid,
             exe_of_pid=exe_of_pid,
         )
@@ -69,16 +79,8 @@ def postprocess_list(windows: "list[WindowInfo]"):
                 break
 
 
-def get_all_windows():
-    # create an empty list to store results
+def get_visible_windows():
     windows: "list[WindowInfo]" = []
-    # call EnumWindows to append all top-level windows to list
     win32gui.EnumWindows(enum_windows_callback, windows)
-
     postprocess_list(windows)
     return windows
-
-
-# print all windows
-for x in get_all_windows():
-    print(x)
