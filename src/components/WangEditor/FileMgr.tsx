f869557308c677fee8fd5b73e9bd8a0ff2a2af81
Jerry Chen <jerryc443@gmail.com>
2023-12-31 18:08:40 -0500

    Add dialog component to FileMgr component

--- src/components/WangEditor/FileMgr.tsx
+++ src/components/WangEditor/FileMgr.tsx
@@ -1,4 +1,12 @@
 import { Button } from '@/shadcnui/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/shadcnui/ui/dialog'
 import { bottomRightBtnClass } from '@/utils'
 import { FolderKanban } from 'lucide-react'
 import React from 'react'
@@ -6,30 +14,43 @@ import React from 'react'
 export const FileMgr = React.memo(
   ({ className }: { className: React.HTMLAttributes<never>['className'] }) => {
     return (
-      <Button
-        className={`${bottomRightBtnClass} ${className}`}
-        variant='outline'
-        onClick={() => {
-          // todo
-          // if (editor == null) return
-          // const blob = new Blob([editor.getHtml()], { type: 'text/html' })
-          // const text = new Blob([editor.getText()], { type: 'text/plain' })
-          // navigator.clipboard
-          //   .write([new ClipboardItem({ [blob.type]: blob, [text.type]: text })])
-          //   .then(() => {
-          //     setStatus(true)
-          //     setTimeout(() => {
-          //       setStatus(null)
-          //     }, 1500)
-          //   })
-          //   .catch(e => {
-          //     // todo toast
-          //     console.error(e)
-          //   })
-        }}
-      >
-        <FolderKanban />
-      </Button>
+      <Dialog>
+        <DialogTrigger>
+          <Button
+            className={`${bottomRightBtnClass} ${className}`}
+            variant='outline'
+            onClick={() => {
+              // todo
+              // if (editor == null) return
+              // const blob = new Blob([editor.getHtml()], { type: 'text/html' })
+              // const text = new Blob([editor.getText()], { type: 'text/plain' })
+              // navigator.clipboard
+              //   .write([new ClipboardItem({ [blob.type]: blob, [text.type]: text })])
+              //   .then(() => {
+              //     setStatus(true)
+              //     setTimeout(() => {
+              //       setStatus(null)
+              //     }, 1500)
+              //   })
+              //   .catch(e => {
+              //     // todo toast
+              //     console.error(e)
+              //   })
+            }}
+          >
+            <FolderKanban />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     )
   }
 )
