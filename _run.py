2f9ee503e42fb1ff1a9aa4da8f7903e483f7ca10
Jerry Chen <jerryc443@gmail.com>
2024-01-03 02:15:39 -0500

    Add run script and remove setup.py

--- _run.py
+++ _run.py
@@ -4,16 +4,27 @@ import subprocess as sp
 import sys
 import time
 from traceback import print_exc
+from pathlib import Path
 from app import main
 
 try:
-    sp.check_call((sys.executable, "-m", "pip", "install", "-r", "requirements.txt"))
+    sp.check_call(
+        (
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "-r",
+            str(Path(__file__).parent / "requirements.txt"),
+        )
+    )
     main()
     print("\n\n按回车键退出！\n\n")
-
+except KeyboardInterrupt:
+    ...
 except Exception:
     print_exc()
     print("\n\n它寄了！！！把错误信息截图发给我！！！\n\n")
 
-time.sleep(5)
+time.sleep(3)
 input()
---
+++ run.cmd
@@ -0,0 +1,2 @@
+@echo off
+python "%~dp0_run.py"
