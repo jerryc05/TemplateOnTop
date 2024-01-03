9f551e49e73c5e4ea368a6f720394fddcc17497b
Jerry Chen <jerryc443@gmail.com>
2024-01-03 01:27:38 -0500

    Add print statements and sleep timer

--- run.py
+++ run.py
@@ -2,14 +2,18 @@
 
 import subprocess as sp
 import sys
+import time
 from traceback import print_exc
 from app import main
 
 try:
     sp.check_call((sys.executable, "-m", "pip", "install", "-r", "requirements.txt"))
     main()
-    input("按回车键退出！")
+    print("\n\n按回车键退出！\n\n")
 
 except Exception:
     print_exc()
-    input("寄了，把错误信息截图发给我！！！")
+    print("\n\n它寄了！！！把错误信息截图发给我！！！\n\n")
+
+time.sleep(5)
+input()
